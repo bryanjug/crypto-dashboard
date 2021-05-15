bbd404406c0a0ec885cc9bd8d7956a8e9cf102ba
bryanjug <bryandjug@gmail.com>
2021-05-14 21:57:31 -0700

    created planning

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import CoinGeko from "coingecko-api";
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import GoogleBtn from "./GoogleBtn";
+
+//() CREATE LEFT NAV
+//() LOGIN FOR FAVORITES PRICE WATCHING
+//() TOP HIGHS AND LOWS FROM PAST 24 HOURS / PAST HOUR / PAST WEEK / PAST YEAR
+//() EXCHANGE RATES (PRICES OF EACH CURRENT AND COIN)
+//() TRENDING COINS
+//() EXCHANGES LIST
+//() COINS LIST
+//() SEARCH BAR FOR PRICE
+//() NEWS
+
+const App = () => {
+    return (
+        <div>App</div>
+    );
+}
+
+export default App;
