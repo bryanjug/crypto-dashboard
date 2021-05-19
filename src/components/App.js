adbeee67439e41337b22990b8e40009413debcb6
bryanjug <bryandjug@gmail.com>
2021-05-18 23:00:31 -0700

    added styling for nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,10 @@ import { Link } from "react-router-dom";
 
 //(DONE) SET UP REDUX AND REDUX THUNK
 //(DONE) BUILD GOOGLE LOGIN BUTTON
-//() RESPONSIVE GOOGLE LOGIN BUTTON
+//() BUILD NAV BAR AND CLOSING / OPENING
+//(DONE) STYLE GOOGLE LOGIN BUTTON
+//() SHOW USER'S NAME ONCE LOGGED
+//() CHANGE CLASSNAME FOR DASHBOARD AND NAV ONCE SCREEN SIZE CHANGES USING STATE
 //() CREATE RESPONSIVE LEFT NAV
 //() LOGIN FOR FAVORITES PRICE WATCHING
 //() TOP HIGHS AND LOWS FROM PAST 24 HOURS / PAST HOUR / PAST WEEK / PAST YEAR
@@ -20,35 +23,51 @@ import { Link } from "react-router-dom";
 
 const App = ({ incrementMyCount, myCount }) => {
 	return (
-		<div className="row">
-            <div className="col-0 navContainer">
-                <div className="logo nav">
-                    <button className="navButton">
+        <div>
+            <div className="openNavButton">
+                <i class="bi bi-list"></i>
+            </div>
+            <div className="navContainer">
+                <div className="nav">
+                    <button className="navButton logo">
+                        <div className="closeNavButton">
+                            <i class="bi bi-x-lg"></i>
+                        </div>
                         <i className="bi bi-currency-bitcoin navIcon logoIcon"></i>
-                        <span className="navText">Crypto</span>
+                        <span className="navText logoText">Crypto</span>
                     </button>
                 </div>
-                <GoogleBtn />
+                <GoogleBtn />        
+                <hr className="navLine" />
                 <Link to="/" className="nav">
-                    <i className="bi bi-speedometer navIcon"></i>
-                    <span className="navText">Dashboard</span>
+                    <button className="navButton">
+                        <i className="bi bi-speedometer navIcon"></i>
+                        <span className="navText">Dashboard</span>
+                    </button>
                 </Link>
                 <Link to="/prices" className="nav">
-                    <i className="bi bi-graph-up navIcon"></i>
-                    <span className="navText">Prices</span>
+                    <button className="navButton">
+                        <i className="bi bi-graph-up navIcon"></i>
+                        <span className="navText">Prices</span>
+                    </button>
                 </Link>
                 <Link to="/events" className="nav">
-                    <i className="bi bi-calendar-check-fill navIcon"></i>
-                    <span className="navText">Events</span>
+                    <button className="navButton">
+                        <i className="bi bi-calendar-check-fill navIcon"></i>
+                        <span className="navText">Events</span>
+                    </button>
                 </Link>
                 <Link to="/exchanges" className="nav">
-                    <i className="bi bi-shield-lock-fill navIcon"></i>
-                    <span className="navText">Exchanges</span>
+                    <button className="navButton">
+                        <i className="bi bi-shield-lock-fill navIcon"></i>
+                        <span className="navText">Exchanges</span>
+                    </button>
                 </Link>
             </div>
-            <div className="col-10 dashboard">
-                <button onClick={incrementMyCount}>{myCount}</button>
-                <br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L<br/>L
+            <div className="row">
+                <div className="col-12 dashboard">
+                    <button onClick={incrementMyCount}>{myCount}</button>
+                </div>
             </div>
 		</div>
 	);
